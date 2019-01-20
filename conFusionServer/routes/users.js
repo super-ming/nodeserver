c01dca4290c03fa43e29c1190c17ab7ac61be4e4
Ming Ho <hkboi108@hotmail.com>
2019-01-19 16:02:59 -0800

    Modified local authentication method

--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -58,12 +58,31 @@ router.post('/signup', cors.corsWithOptions, (req, res, next) => {
 });
 
 //if authenticated, passport automatically adds user property to req so you can then use req.user
-router.post('/login', cors.corsWithOptions, passport.authenticate('local'),(req, res) => {
-  //create token by searching for user by id in database
-  let token = authenticate.getToken({_id: req.user._id});
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, token: token, status: 'You\'re successfully logged in!'});
+router.post('/login', cors.corsWithOptions, (req, res) => {
+  //info is filled if the user doesn't exist as it's not a real error
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    //if passport can't find the user or password
+    if (!user) {
+      res.statusCode = 401;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({success: false, status: 'Login unsuccessful!', err: info});
+    }
+    req.logIn(user, err => {
+      if (err) {
+        res.statusCode = 401;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({success: false, status: 'Login unsuccessful!', err: 'Could not log in'});
+      }
+      //create token by searching for user by id in database
+      let token = authenticate.getToken({_id: req.user._id});
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({success: true, token: token, status: 'Login Successful!'});
+    });
+  })(req, res, next);
 });
 
 //use get instead of post because user doesn't need to supply any info in the body of the message
@@ -92,4 +111,21 @@ router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res
   }
 });
 
+//check if token is still valid
+router.get('/checkJWTtoken', corsWithOptions, (req, res) => {
+  passport.authenticate('jwt', {session: false}, (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      res.statusCode = 401;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({status: 'JWT invalid', success: false, err: info})
+    } else {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({success: true, user: user, status: 'JWT valid'});
+    }
+  })(req, res);
+})
 module.exports = router;
